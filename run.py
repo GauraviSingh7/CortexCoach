1bfa3cfc089f3c8ce855c657d8f3ebf87d336cb6
GauraviSingh7 <gauravi.074@gmail.com>
2025-07-02 12:28:53 +0530

    Untrack server files and add to .gitignore

--- run.py
+++
@@ -1,7 +0,0 @@
-from core.orchestrator import SystemOrchestrator
-
-if __name__ == "__main__":
-    orchestrator = SystemOrchestrator()
-    orchestrator.initialize_all()
-    orchestrator.check_health()
-    print("🎉 System is ready to launch.")
